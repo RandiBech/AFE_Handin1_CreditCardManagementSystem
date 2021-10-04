ad36fae795df41d3d42a5f4c1a43694d1949c0c2
Randi Mikkelsen Bech <au615785@uni.au.dk>
2021-10-04 11:21:01 +0200

    Add navigation bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [{
+  path: 'home',
+  component: HomeComponent
+},{
+  path: '',
+  redirectTo: 'home',
+  pathMatch: 'full'
+},{
   path: 'transaction',
   loadChildren: () => import('./transaction/transaction.module').then(m => m.TransactionModule),
+},{
+  path: 'addCreditCard',
+  loadChildren: () => import('./credit-card/credit-card.module').then(m => m.CreditCardModule),
 }];
 
 @NgModule({
