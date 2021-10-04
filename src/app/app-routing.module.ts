83d93ebf9793474f318ca5d6a1a3ec21efe35ad4
Mads Bo Mikkelsen <au615041@uni.au.dk>
2021-10-04 10:56:56 +0200

    F2.1 done F2.1.1 done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{
+const routes: Routes = [
+  {
+    path: 'creditcards',
+    loadChildren: () => import('./credit-card/credit-card.module').then(m => m.CreditCardModule)
+  },
+  {
   path: 'transaction',
   loadChildren: () => import('./transaction/transaction.module').then(m => m.TransactionModule),
-}];
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/credit-card/credit-card-routing.module.ts
+++ src/app/credit-card/credit-card-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreditCardDetailsComponent } from './credit-card-details/credit-card-details.component';
+import { CreditCardListComponent } from './credit-card-list/credit-card-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: CreditCardListComponent},
+  {path: ':id', component: CreditCardDetailsComponent}
+];
 
-@NgModule({
+@NgModule({ 
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
