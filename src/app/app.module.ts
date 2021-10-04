b0a9df99fa98a896833f7990a9721b2879cd6520
Mads Bo Mikkelsen <au615041@uni.au.dk>
2021-10-04 12:24:08 +0200

    add selector for credit card to home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NagivationBarComponent } from './nagivation-bar/nagivation-bar.component';
+import { CreditCardListComponent } from './credit-card/credit-card-list/credit-card-list.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, NagivationBarComponent],
+  declarations: [AppComponent, HomeComponent, NagivationBarComponent, CreditCardListComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, FlexLayoutModule],
   providers: [],
   bootstrap: [AppComponent],
