9e14dd5f424a98e9b26198ec48a048ee1ceab21e
Quynh Trang Mac <201810269@post.au.dk>
2021-10-11 20:21:08 +0200

    finish w. filtering

--- src/app/credit-card/credit-card-details/credit-card-details.component.ts
+++ src/app/credit-card/credit-card-details/credit-card-details.component.ts
@@ -1,32 +1,36 @@
-import { Component, EventEmitter, Inject, Input, OnDestroy, OnInit, Output } from '@angular/core';
-import { ActivatedRoute, ParamMap , Router} from '@angular/router';
+import {
+  Component,
+  EventEmitter,
+  Inject,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { Observable, Subscription } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { CreditCardService } from 'src/app/credit-card.service';
-import {Card} from 'src/app/card-type';
+import { Card } from 'src/app/card-type';
 
 @Component({
   selector: 'app-credit-card-details',
   templateUrl: './credit-card-details.component.html',
-  styleUrls: ['./credit-card-details.component.scss']
+  styleUrls: ['./credit-card-details.component.scss'],
 })
 export class CreditCardDetailsComponent implements OnInit, OnDestroy {
-
   creditCard!: Card;
   @Output() deleteRequest = new EventEmitter<Card>();
   sub!: Subscription;
-  
 
-  constructor(private route: ActivatedRoute) 
-  {  }
+  constructor(private route: ActivatedRoute) {}
 
-  ngOnInit(): void 
-  {  
-    this.sub = this.route.paramMap.subscribe(params => {
+  ngOnInit(): void {
+    this.sub = this.route.paramMap.subscribe((params) => {
       let id = params.get('id');
       this.getCard();
-    }); 
-    
+    });
+
     console.log('state', history.state);
     console.log('creditcard: ', this.creditCard);
   }
